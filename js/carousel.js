b8cbe0f11befff22a9e2fb98234f96d0fca44bfc
eleyna <eleyna@gmail.com>
2013-07-24 22:59:53 -0400

    done

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,29 @@
 var carousel = document.getElementById('carousel');
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
+var slideshow = document.getElementById('slideshow');
 
 carousel.style.marginLeft = 0;
+
+function nextImage() {
+  var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
+    if (leftPosition === -1224) {
+      carousel.style.marginLeft = "0px";
+    } else {
+      leftPosition += -612;
+      carousel.style.marginLeft = leftPosition  +"px";
+    }
+}
+
+function previousImage() {
+  var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
+    if (leftPosition === 0) {
+      carousel.style.marginLeft = "-1224px";
+    } else {
+      leftPosition += 612;
+      carousel.style.marginLeft = leftPosition  +"px";
+    }
+}
 
 window.onload = function() {
 
@@ -23,22 +44,18 @@ window.onload = function() {
   };
 
   next.onclick = function(event) {
-    var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
-    if (leftPosition === -1224) {
-      carousel.style.marginLeft = "0px";
-    } else {
-      leftPosition += -612;
-      carousel.style.marginLeft = leftPosition  +"px";
-    }
+    nextImage();
   };
 
   previous.onclick = function(event){
-    var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
-    if (leftPosition === 0) {
-      carousel.style.marginLeft = "-1224px";
-    } else {
-      leftPosition += 612;
-      carousel.style.marginLeft = leftPosition  +"px";
-    }
+    previousImage();
+  };
+
+  slideshow.onclick = function(event) {
+    intervalID = setInterval(nextImage, 5000);
   };
 };
+
+
+
+
