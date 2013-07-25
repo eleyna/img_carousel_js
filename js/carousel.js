dcba22b97e9a4867e794f081808d0b130e480524
eleyna <eleyna@gmail.com>
2013-07-24 21:24:42 -0400

    added opacity, and can click through images

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,44 @@
 var carousel = document.getElementById('carousel');
-
-// Buttons
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 
 carousel.style.marginLeft = 0;
 
-// Create mouseoveer and mouseexit events for the buttons
-// to change their opacity from .5 to .2
+window.onload = function() {
 
-// Create onclick events to advance the images
+  next.onmouseover = function(event){
+    this.style.opacity = 0.5;
+  };
+
+  previous.onmouseover = function(event){
+    this.style.opacity = 0.5;
+  };
+
+  next.onmouseout = function(event){
+    this.style.opacity = 0.2;
+  };
+
+  previous.onmouseout = function(event){
+    this.style.opacity = 0.2;
+  };
+
+  next.onclick = function(event) {
+    var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
+    if (leftPosition === -1224) {
+      carousel.style.marginLeft = "0px";
+    } else {
+      leftPosition += -612;
+      carousel.style.marginLeft = leftPosition  +"px";
+    }
+  };
+
+  previous.onclick = function(event){
+    var leftPosition = parseInt(carousel.style.marginLeft.replace("px",""), 10);
+    if (leftPosition === 0) {
+      carousel.style.marginLeft = "-1224px";
+    } else {
+      leftPosition += 612;
+      carousel.style.marginLeft = leftPosition  +"px";
+    }
+  };
+};
